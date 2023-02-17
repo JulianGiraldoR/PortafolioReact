f7ad6282ac947cd74956204865562125b29bc3fc
Julian Giraldo <90876994+JulianGiraldoR@users.noreply.github.com>
2023-02-17 12:41:30 +0000

    links repos para portfolio

--- src/components/work/WorkItems.jsx
+++ src/components/work/WorkItems.jsx
@@ -5,8 +5,9 @@ const WorkItems = ({item}) => {
         <div className="work__card" key={item.id}>
             <img src={item.image} alt='' className='work__img' />
             <h3 className="work__title">{item.title}</h3>
-            <a href="https://github.com/JulianGiraldoR?tab=repositories" target="_blank" className="work__button">
-            Repository<i className="bx bx-right-arrow-alt work__button-icon"></i>
+           
+            <a href={item.url} target="_blank" className="work__button">Repository
+            <i className="bx bx-right-arrow-alt work__button-icon"></i>
             </a>
         </div>
     )
